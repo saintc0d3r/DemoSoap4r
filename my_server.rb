161e9893b6920345d9282ea23059306dbf1439c4
Wendy Sanarwanto <saintc0d3r@gmail.com>
2012-03-04 20:56:09 +0800

    Added my_server.rb
    Signed-off-by: Wendy Sanarwanto <saintc0d3r@gmail.com>

---
+++ my_server.rb
@@ -0,0 +1,29 @@
+require "soap/rpc/standaloneServer"
+
+class MyServer < SOAP::RPC::StandaloneServer
+# Expose our services
+  def initialize(*args)
+      super(args[0], args[1], args[2], args[3])
+      add_rpc_method(self, 'add', 'a', 'b')
+      add_rpc_method(self, 'div', 'a', 'b')
+  end
+
+  # Handler methods
+  def add(a, b)
+     return a + b
+  end
+  def div(a, b)
+     return a / b
+  end
+
+end
+
+begin
+  server = MyServer.new("MyServer",'urn:ruby:calculation', 'localhost', 9999)
+  trap('INT'){
+    server.shutdown
+  }
+  server.start
+  rescue => err
+  puts err.message
+end
